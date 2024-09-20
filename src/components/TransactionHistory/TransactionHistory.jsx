49397636b0324d3838a6df8cbaed8fff62a13cc7
Vladyslav Natalych <v.natalich05@gmail.com>
2024-09-20 16:41:50 +0300

    Fix vercel transactionHistory css bug

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,22 +1,22 @@
-import "./TransactionHistory.module.css"
+import css from './TransactionHistory.module.css'
 const TransactionHistory = ({ items }) => {
   return (
-    <table>
-  <thead>
-    <tr>
-      <th>Type</th>
-      <th>Amount</th>
-      <th>Currency</th>
+    <table className={css.transTable}>
+  <thead className={css.transThead}>
+    <tr className={css.transTr}>
+      <th className={css.transTh}>Type</th>
+      <th className={css.transTh}>Amount</th>
+      <th className={css.transTh}>Currency</th>
     </tr>
   </thead>
 
   <tbody>
     {items.map(item =>{
         return (
-    <tr key={item.id}>
-      <td>{item.type}</td>
-      <td>{item.amount}</td>
-      <td>{item.currency}</td>
+    <tr className={css.transTr} key={item.id}>
+      <td className={css.transTd}>{item.type}</td>
+      <td className={css.transTd}>{item.amount}</td>
+      <td className={css.transTd}>{item.currency}</td>
     </tr>
         );
     })}
